010e90120924cf27cbaf0bd89f193fb29482db15
Dani Buchsbaum <danibuchsbaum@Danis-MacBook-Air.local>
2024-08-11 17:01:21 +0300

    First live version

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from forms import Register
 import os
 from dotenv import load_dotenv
 import smtplib
+import bleach
+import secrets
 
 load_dotenv()
 
@@ -19,14 +21,22 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("FLASK_KEY")
 Bootstrap5(app)
 
+
 app.config['SEND_EMAIL'] = os.getenv("SEND_EMAIL")
 app.config['EMAIL_PASS'] = os.getenv("EMAIL_PASS")
-app.config['EMAIL_RECEIVE'] = os.getenv("EMAIL_RECEIVE")
+app.config['EMAIL_RECEIVE'] = os.getenv("SEND_EMAIL")
+app.config['SESSION_COOKIE_SECURE'] = True
 
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
     return render_template("index.html")
+
+
+# THANK YOU PAGE
+@app.route('/thank-you', methods=['POST', 'GET'])
+def thank_you():
+    return render_template("thank_you.html")
 
 
 # REGISTER INTEREST
@@ -35,16 +45,67 @@ def register():
     if request.method == 'POST':
         form_data = request.form
         email_entered = form_data['email']
-        with smtplib.SMTP("smtp.gmail.com") as connection:
-            connection.starttls()
-            connection.login(user=app.config['SEND_EMAIL'], password=app.config['EMAIL_PASS'])
-            connection.sendmail(
-                from_addr=app.config['SEND_EMAIL'], 
-                to_addrs=app.config['EMAIL_RECEIVE'], 
-                msg=f"Subject: New Poker Registration\n\nThere is a new person interested in the website!\nEmail: {email_entered}"
-                )
-            return redirect(url_for('home'))
+        name_entered = form_data['name']
+        try:
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user=app.config['SEND_EMAIL'], password=app.config['EMAIL_PASS'])
+                connection.sendmail(
+                    from_addr=app.config['SEND_EMAIL'], 
+                    to_addrs=app.config['EMAIL_RECEIVE'], 
+                    msg=f"Subject: New Poker Registration\n\n{name_entered} is interested in the full version of the website!\nEmail: {email_entered}"
+                    )
+        except Exception as e:
+            return f"An error occurred: {e}", 500
+        
+        return redirect(url_for('thank_you'))
     return render_template("register.html")
+
+
+# CONTACT US
+@app.route('/contact-us', methods=['POST', 'GET'])
+def contact_form():
+    if request.method == 'POST':
+        form_data = request.form
+        email_entered = form_data['email']
+        name_entered = form_data['name']
+        message = form_data['message']
+        try:
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user=app.config['SEND_EMAIL'], password=app.config['EMAIL_PASS'])
+                connection.sendmail(
+                    from_addr=app.config['SEND_EMAIL'], 
+                    to_addrs=app.config['EMAIL_RECEIVE'],
+                    msg=f"Subject: New Contact Form Message\n\n{name_entered} has sent a message!\nEmail: {email_entered}\n"
+                            f"Message: {message}"
+                    )
+        except Exception as e:
+            return f"An error occurred: {e}", 500
+
+    return render_template("contact_form.html")
+
+
+# CONTACT FOOTER
+@app.route('/feature-request', methods=['POST', 'GET'])
+def contact_footer():
+    if request.method == 'POST':
+        form_data = request.form
+        message = form_data['message']
+        try:
+            with smtplib.SMTP("smtp.gmail.com") as connection:
+                connection.starttls()
+                connection.login(user=app.config['SEND_EMAIL'], password=app.config['EMAIL_PASS'])
+                connection.sendmail(
+                    from_addr=app.config['SEND_EMAIL'], 
+                    to_addrs=app.config['EMAIL_RECEIVE'], 
+                    msg=f"Subject: New Poker Message\n\nYou were sent the following message:\n{message}"
+                )
+        except Exception as e:
+            return f"An error occurred: {e}", 500
+            
+        return redirect(request.referrer)
+
 
 
 # FREE VERSION - NUMBER OF PLAYERS
@@ -76,10 +137,7 @@ def free_details():
             session.pop('extra_data', None)
             return redirect(url_for('free_results', players=num_players))
         else:
-            flash('Total cash-outs do not equal total buy-ins!')
-            flash(f'Total buy-ins: {buy_ins}')
-            flash(f'Total cash-outs: {cash_outs}')
-            flash(f'Difference: {abs(buy_ins-cash_outs)}')
+            flash(f'Calculation error  |  Buy-ins: {buy_ins}  |  Cash-outs: {cash_outs}  |  Difference: {abs(buy_ins-cash_outs)}')
             session['form_data'] = data.to_dict()
             return redirect(url_for('free_details', players=num_players))
     form_data = session.get('form_data', {})
@@ -87,27 +145,42 @@ def free_details():
     return render_template("player_details.html", players=number_of_players, form_data=form_data)
 
 
+# CLEAR DETAILS TABLE
+@app.route('/clear-table', methods=['POST', 'GET'])
+def clear_table():
+    session.pop('form_data', None)
+    return redirect(url_for('free_details'))
+
+
 # FREE VERSION - RESULTS
 @app.route('/results', methods=['POST', 'GET'])
 def free_results():
 
-    if request.method == 'POST':
+    if request.method == 'POST':        
         # Get new form data
         new_data = request.form.to_dict()
+        payer = new_data['payer'].capitalize()
+        payee = new_data['payee'].capitalize()
+
+        form_data = session.get('form_data', {})
+        num_players = session.get('num_players')
+
+        player_names = [form_data[f'player_{i}'].capitalize() for i in range(num_players)]
         
-        # Retrieve existing data from session or initialize as an empty list
-        previous_data = session.get('extra_data', [])
-        
-        # Append new data to the list
-        previous_data.append(new_data)
-        
-        # Save updated data back to the session
-        session['extra_data'] = previous_data
-        
-        return redirect(url_for("free_results"))
+        if payer not in player_names or payee not in player_names:
+            flash("One or more of the names entered were not involved in the game.")
+            return redirect(url_for("free_results"))
+        else: 
+            # Update session with new data
+            previous_data = session.get('extra_data', [])
+            previous_data.append(new_data)
+            session['extra_data'] = previous_data
+            
+            return redirect(url_for("free_results"))
     
     num_players = session.get('num_players')
     form_data = session.get('form_data', {})
+
     players = []
     for i in range(num_players):
         player = {
@@ -116,6 +189,9 @@ def free_results():
             'cashout': float(form_data[f'cashout_{i}'])
         }
         players.append(player)
+
+    # Caluculation for winnings
+    session_winnings = [player['cashout'] - player['buyin'] for player in players]
     
     extra_data = session.get('extra_data', [])
     help_needed = False
@@ -128,7 +204,7 @@ def free_results():
 
     return render_template("results.html", form_data=form_data, 
                            players=num_players, settlements=settlements,
-                           help_needed=help_needed)
+                           help_needed=help_needed, winnings=session_winnings)
 
 
 def calculate_settlements(players):
